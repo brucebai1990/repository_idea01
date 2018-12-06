5f413c867aaf6e4dbef9f264046ff47a4d19beda
brucebai1990 <brucebai1990@163.com>
2018-12-06 17:34:51 +0800

    提交工程git_test_project到本地仓库

---
+++ git_test_project/src/com/itheima/helloworld.java
@@ -0,0 +1,11 @@
+package com.itheima;
+
+public class helloworld {
+    public static void main(String[] args) {
+        System.out.println("hello world ....");
+        System.out.println("hello world ....");
+        System.out.println("hello world ....");
+        System.out.println("hello world ....");
+        System.out.println("hello world ....");
+    }
+}
